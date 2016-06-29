1e993ccd45ea5a0a5e59c7ac675a954f501db44a
vissu4073 <vissu_nbkr@yahoo.com>
2016-06-29 10:32:20 +0530

    commit

--- webapp/src/test/resources/workspace/widgets/pbLabel/pbLabel.js
+++
@@ -1,6 +0,0 @@
-angular.module('bonitasoft.ui.widgets')
-  .directive('pbLabel', function() {
-    return {
-      template: '<div class="text-{{ properties.alignment }}"><label>{{ properties.text }}</label></div>\n'
-    };
-  });
